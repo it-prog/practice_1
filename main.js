18633d0cbb11ea49f932cb0406f8e5b2e50caf12
Vladislav <lapshin.vlad96@gmail.com>
2022-11-03 14:06:47 +0200

    the first task was done

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Назвіть один із останніх переглянутих фільмів?', ''),
+      b = prompt('Яку оцінку ви йому поставите?', ''),
+      c = prompt('Назвіть один із останніх переглянутих фільмів?', ''),
+      d = prompt('Яку оцінку ви йому поставите?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
