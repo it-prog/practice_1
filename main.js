dabd15a9701e76d2f54f9f96fd1754fb18c6cfae
Vladislav <lapshin.vlad96@gmail.com>
2022-11-04 14:06:32 +0200

    was done second task of project

--- main.js
+++ main.js
@@ -10,13 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Назвіть один із останніх переглянутих фільмів?', ''),
-      b = prompt('Яку оцінку ви йому поставите?', ''),
-      c = prompt('Назвіть один із останніх переглянутих фільмів?', ''),
-      d = prompt('Яку оцінку ви йому поставите?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Назвіть один із останніх переглянутих фільмів?', ''),
+          b = prompt('Яку оцінку ви йому поставите?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Ви подивились доволі невелику кількість фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ви реальний кіноман');
+} else {
+    console.log('Вибачте, сталася помилка');
+}
 
 console.log(personalMovieDB);
 
